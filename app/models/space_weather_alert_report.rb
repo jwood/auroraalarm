b82c908d2b451c32b573ef4c5be520ff0d2a3cd3
John Wood <john@johnpwood.net>
2012-07-17 09:42:16 -0500

    Change expected_kp_index to kp_index
    For alert events, the kp index reported is the actual kp index, not an
    expected kp index (like it is with warnings).

--- app/models/space_weather_alert_report.rb
+++ app/models/space_weather_alert_report.rb
@@ -1,12 +1,12 @@
 class SpaceWeatherAlertReport
   class SpaceWeatherEvent
-    attr_reader :message_code, :serial_number, :issue_time, :expected_kp_index
+    attr_reader :message_code, :serial_number, :issue_time, :kp_index
 
-    def initialize(message_code, serial_number, issue_time, expected_kp_index)
+    def initialize(message_code, serial_number, issue_time, kp_index)
       @message_code = message_code
       @serial_number = serial_number
       @issue_time = Time.parse(issue_time)
-      @expected_kp_index = expected_kp_index.to_i
+      @kp_index = kp_index.to_i
     end
   end
 
@@ -45,14 +45,14 @@ class SpaceWeatherAlertReport
   end
 
   def parse_section(section)
-    message_code = serial_number = issue_time = expected_kp_index = nil
+    message_code = serial_number = issue_time = kp_index = nil
     section.each do |line|
       message_code = $1 if line =~ /^Space Weather Message Code: (.*)<br>/
       serial_number = $1 if line =~ /^Serial Number: (.*)<br>/
       issue_time = "#{$1} #{$2} #{$3[0..1]}:#{$3[2..3]} #{$4}" if line =~ /^Issue Time: (\d\d\d\d) (.*) (\d\d\d\d) (.*)<br>/
-      expected_kp_index = $1 if line =~ /Geomagnetic K-index of (.*) expected<br>/
+      kp_index = $1 if line =~ /Geomagnetic K-index of (\d+).*<br>/
     end
-    SpaceWeatherEvent.new(message_code, serial_number, issue_time, expected_kp_index)
+    SpaceWeatherEvent.new(message_code, serial_number, issue_time, kp_index)
   end
 
 end
--- test/unit/space_weather_alert_report_test.rb
+++ test/unit/space_weather_alert_report_test.rb
@@ -15,25 +15,25 @@ class SpaceWeatherAlertReportTest < ActiveSupport::TestCase
     assert_equal "WARK04", event.message_code
     assert_equal "1954", event.serial_number
     assert_equal Time.utc(2012, 6, 30, 23, 49), event.issue_time
-    assert_equal 4, event.expected_kp_index
+    assert_equal 4, event.kp_index
 
     event = events[5]
     assert_equal "ALTEF3", event.message_code
     assert_equal "1923", event.serial_number
     assert_equal Time.utc(2012, 6, 24, 10, 11), event.issue_time
-    assert_equal 0, event.expected_kp_index
+    assert_equal 0, event.kp_index
 
     event = events[81]
     assert_equal "WARK04", event.message_code
     assert_equal "1935", event.serial_number
     assert_equal Time.utc(2012, 6, 02, 22, 24), event.issue_time
-    assert_equal 4, event.expected_kp_index
+    assert_equal 4, event.kp_index
 
     event = events[82]
     assert_equal "ALTTP2", event.message_code
     assert_equal "796", event.serial_number
     assert_equal Time.utc(2012, 6, 1, 23, 14), event.issue_time
-    assert_equal 0, event.expected_kp_index
+    assert_equal 0, event.kp_index
   end
 
   should "be able to find all events for a given date" do
@@ -44,13 +44,19 @@ class SpaceWeatherAlertReportTest < ActiveSupport::TestCase
     assert_equal "WARK04", event.message_code
     assert_equal "1950", event.serial_number
     assert_equal Time.utc(2012, 6, 17, 23, 54), event.issue_time
-    assert_equal 4, event.expected_kp_index
+    assert_equal 4, event.kp_index
+
+    event = events[5]
+    assert_equal "ALTK06", event.message_code
+    assert_equal "286", event.serial_number
+    assert_equal Time.utc(2012, 6, 17, 11, 33), event.issue_time
+    assert_equal 6, event.kp_index
 
     event = events[10]
     assert_equal "WARK04", event.message_code
     assert_equal "1947", event.serial_number
     assert_equal Time.utc(2012, 6, 17, 2, 32), event.issue_time
-    assert_equal 4, event.expected_kp_index
+    assert_equal 4, event.kp_index
   end
 
 end
