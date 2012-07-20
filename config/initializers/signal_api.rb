7bde5f51d4f826bdf8120bc81a3ac2ef5ace58f5
John Wood <john@johnpwood.net>
2012-07-20 17:09:35 -0500

    Add api_key and logger configurations for the Signal API

--- config/initializers/signal_api.rb
+++ config/initializers/signal_api.rb
@@ -2,3 +2,6 @@ if Rails.env.test?
   ENV['SIGNAL_DELIVER_SMS_USERNAME'] = "username"
   ENV['SIGNAL_DELIVER_SMS_PASSWORD'] = "password"
 end
+
+SignalApi.api_key = ENV['SIGNAL_API_KEY']
+SignalApi.logger = Rails.logger
