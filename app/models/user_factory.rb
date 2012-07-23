5e0f9498aa0b08c58734397d4757d3ef61955f1d
John Wood <john@johnpwood.net>
2012-07-22 19:52:54 -0500

    Add new factory to manage creating the User and UserLocation objects

---
+++ app/models/user_factory.rb
@@ -0,0 +1,59 @@
+class UserFactory
+  attr_accessor :errors
+
+  def create_user(mobile_phone, location_value)
+    @errors = []
+
+    ensure_valid_phone(mobile_phone)
+    ensure_user_does_not_exist(mobile_phone)
+    location = lookup_location_data(location_value)
+
+    user = nil
+    if @errors.blank?
+      begin
+        User.transaction do
+          user = User.create!(:mobile_phone => mobile_phone)
+          user_location = UserLocation.create!(:user_id => user.id,
+                                               :city => location.city,
+                                               :state => location.state,
+                                               :postal_code => location.zip,
+                                               :country => location.country_code,
+                                               :latitude => location.latitude,
+                                               :longitude => location.longitude,
+                                               :magnetic_latitude => location.magnetic_latitude)
+        end
+      rescue Exception => e
+        @errors << "An unexpected error occurred while trying to create a new user"
+        Rails.logger.error "An unexpected error occurred when trying to create a new user : #{e.message}"
+      end
+    end
+    user
+  end
+
+  private
+
+  def ensure_valid_phone(mobile_phone)
+    if !SignalApi::Phone.valid?(mobile_phone)
+      @errors << "Mobile phone is invalid"
+    end
+  end
+
+  def ensure_user_does_not_exist(mobile_phone)
+    if User.exists?(:mobile_phone => mobile_phone)
+      @errors << "A user with the specified mobile phone already exists"
+    end
+  end
+
+  def lookup_location_data(location_value)
+    service = GeolocationService.new
+    location = service.geocode(location_value)
+
+    if location.latitude.blank? || location.longitude.blank?
+      @errors << "Location is invalid"
+      nil
+    else
+      location
+    end
+  end
+
+end
---
+++ test/unit/user_factory_test.rb
@@ -0,0 +1,67 @@
+require 'test_helper'
+
+class UserFactoryTest < ActiveSupport::TestCase
+
+  def setup
+    @factory = UserFactory.new
+  end
+
+  test "should be able to create a new user with location data" do
+    expects_valid_location("60477")
+
+    user = nil
+    assert_difference 'User.count', 1 do
+      assert_difference 'UserLocation.count', 1 do
+        user = @factory.create_user('3125551212', '60477')
+      end
+    end
+
+    assert @factory.errors.empty?
+
+    assert_equal '3125551212', user.mobile_phone
+    assert_equal 'Tinley Park', user.user_location.city
+    assert_equal 'IL', user.user_location.state
+    assert_equal '60477', user.user_location.postal_code
+    assert_equal 'US', user.user_location.country
+    assert_equal 41.5699614, user.user_location.latitude
+    assert_equal -87.7861711, user.user_location.longitude
+    assert_equal 52, user.user_location.magnetic_latitude
+  end
+
+  test "should not create the user if the mobile phone is invalid" do
+    expects_valid_location("60477")
+
+    assert_no_difference 'User.count' do
+      assert_no_difference 'UserLocation.count' do
+        assert_nil @factory.create_user('foobar', '60477')
+      end
+    end
+
+    assert_equal ["Mobile phone is invalid"], @factory.errors
+  end
+
+  test "should not create the user if the mobile phone is in use" do
+    expects_valid_location("60477")
+
+    assert_no_difference 'User.count' do
+      assert_no_difference 'UserLocation.count' do
+        assert_nil @factory.create_user(users(:john).mobile_phone, '60477')
+      end
+    end
+
+    assert_equal ["A user with the specified mobile phone already exists"], @factory.errors
+  end
+
+  test "should not create a new user of the location provided is invalid" do
+    expects_invalid_location("zzz")
+
+    assert_no_difference 'User.count' do
+      assert_no_difference 'UserLocation.count' do
+        assert_nil @factory.create_user('3125551212', 'zzz')
+      end
+    end
+
+    assert_equal ["Location is invalid"], @factory.errors
+  end
+
+end
