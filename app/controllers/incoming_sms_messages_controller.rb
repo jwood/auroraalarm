5b4bee680c3af72094637a37e4680a612c3d43b8
John Wood <john@johnpwood.net>
2012-07-23 08:04:24 -0500

    Use HTTP basic auth to restrict access to the incoming sms controller

--- app/controllers/incoming_sms_messages_controller.rb
+++ app/controllers/incoming_sms_messages_controller.rb
@@ -1,4 +1,5 @@
 class IncomingSmsMessagesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['SIGNAL_RECEIVE_SMS_USERNAME'], :password => ENV['SIGNAL_RECEIVE_SMS_PASSWORD']
 
   def index
     @mobile_phone = params[:mobile_phone]
--- test/functional/incoming_sms_messages_controller_test.rb
+++ test/functional/incoming_sms_messages_controller_test.rb
@@ -2,6 +2,16 @@ require 'test_helper'
 
 class IncomingSmsMessagesControllerTest < ActionController::TestCase
 
+  def setup
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(ENV['SIGNAL_RECEIVE_SMS_USERNAME'], ENV['SIGNAL_RECEIVE_SMS_PASSWORD'])
+  end
+
+  test "should deny access if credentials are incorrect" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('foo', 'bar')
+    post :index, :mobile_phone => '9999999999', :message => 'foo bar'
+    assert_response :unauthorized
+  end
+
   test "should not freak out if we get a message for a mobile phone number we do not know about" do
     post :index, :mobile_phone => '9999999999', :message => 'foo bar'
     assert_response :success
