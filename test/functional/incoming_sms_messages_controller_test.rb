bf3e53cfdd9a670e437d005dba7b60f7d12466e1
John Wood <john@johnpwood.net>
2012-07-25 17:54:15 -0500

    Beef up the incoming sms messages tests

--- test/functional/incoming_sms_messages_controller_test.rb
+++ test/functional/incoming_sms_messages_controller_test.rb
@@ -21,35 +21,40 @@ class IncomingSmsMessagesControllerTest < ActionController::TestCase
     user = users(:john)
     SmsMessagingService.any_instance.expects(:send_message).with(user.mobile_phone, OutgoingSmsMessages.signup_confirmation)
 
-    post :index, :mobile_phone => user.mobile_phone, :message => ' y '
+    assert_no_new_user do
+      post :index, :mobile_phone => user.mobile_phone, :message => ' y '
+    end
     assert_response :success
     assert user.reload.confirmed?
-  end
-
-  test "should ask the user for their location if only the keyword is sent" do
-    SmsMessagingService.any_instance.expects(:send_message).with('3125551213', OutgoingSmsMessages.location_prompt)
-    post :index, :mobile_phone => '3125551213', :message => ' aurora', :keyword => 'AURORA'
-    assert_response :success
   end
 
   test "should be able to signup by texting AURORA with the zip code" do
     expects_valid_location("90210")
     SmsMessagingService.any_instance.expects(:send_message).with('3125551213', OutgoingSmsMessages.signup_confirmation)
-    post :index, :mobile_phone => '3125551213', :message => ' aurora 90210 ', :keyword => 'AURORA'
+
+    assert_new_user do
+      post :index, :mobile_phone => '3125551213', :message => ' aurora 90210 ', :keyword => 'AURORA'
+    end
     assert_response :success
   end
 
   test "should send an error if we cannot recognize the location" do
     expects_invalid_location("FOOBAZ")
     SmsMessagingService.any_instance.expects(:send_message).with('3125551213', OutgoingSmsMessages.bad_location_at_signup)
-    post :index, :mobile_phone => '3125551213', :message => ' aurora foobaz ', :keyword => 'AURORA'
+
+    assert_no_new_user do
+      post :index, :mobile_phone => '3125551213', :message => ' aurora foobaz ', :keyword => 'AURORA'
+    end
     assert_response :success
   end
 
   test "shoud let the user know if they are already signed up" do
     user = users(:dan)
     SmsMessagingService.any_instance.expects(:send_message).with(user.mobile_phone, OutgoingSmsMessages.already_signed_up)
-    post :index, :mobile_phone => user.mobile_phone, :message => ' aurora', :keyword => 'AURORA'
+
+    assert_no_new_user do
+      post :index, :mobile_phone => user.mobile_phone, :message => ' aurora', :keyword => 'AURORA'
+    end
     assert_response :success
   end
 
@@ -57,12 +62,9 @@ class IncomingSmsMessagesControllerTest < ActionController::TestCase
     user = users(:dan)
     SmsMessagingService.any_instance.expects(:send_message).with(user.mobile_phone, OutgoingSmsMessages.stop)
 
-    assert_difference 'User.count', -1 do
-      assert_difference 'UserLocation.count', -1 do
-        post :index, :mobile_phone => user.mobile_phone, :message => ' stop', :keyword => ''
-      end
+    assert_user_deleted do
+      post :index, :mobile_phone => user.mobile_phone, :message => ' stop', :keyword => ''
     end
-
     assert_response :success
   end
 
@@ -70,24 +72,18 @@ class IncomingSmsMessagesControllerTest < ActionController::TestCase
     user = users(:john)
     SmsMessagingService.any_instance.expects(:send_message).with(user.mobile_phone, OutgoingSmsMessages.stop)
 
-    assert_difference 'User.count', -1 do
-      assert_difference 'UserLocation.count', -1 do
-        post :index, :mobile_phone => user.mobile_phone, :message => ' stop', :keyword => ''
-      end
+    assert_user_deleted do
+      post :index, :mobile_phone => user.mobile_phone, :message => ' stop', :keyword => ''
     end
-
     assert_response :success
   end
 
   test "should send unknown user the stop message when they text STOP" do
     SmsMessagingService.any_instance.expects(:send_message).with('3125551213', OutgoingSmsMessages.stop)
 
-    assert_no_difference 'User.count' do
-      assert_no_difference 'UserLocation.count' do
-        post :index, :mobile_phone => '3125551213', :message => ' stop all', :keyword => ''
-      end
+    assert_no_new_user do
+      post :index, :mobile_phone => '3125551213', :message => ' stop all', :keyword => ''
     end
-
     assert_response :success
   end
 
@@ -98,12 +94,9 @@ class IncomingSmsMessagesControllerTest < ActionController::TestCase
     expects_valid_location("60477")
     SmsMessagingService.any_instance.expects(:send_message).with(user.mobile_phone, OutgoingSmsMessages.location_update("60477"))
 
-    assert_no_difference 'User.count' do
-      assert_no_difference 'UserLocation.count' do
-        post :index, :mobile_phone => user.mobile_phone, :message => ' aurora 60477 ', :keyword => 'AURORA'
-      end
+    assert_no_new_user do
+      post :index, :mobile_phone => user.mobile_phone, :message => ' aurora 60477 ', :keyword => 'AURORA'
     end
-
     assert_response :success
     assert_equal "60477", user.user_location.reload.postal_code
   end
@@ -115,14 +108,37 @@ class IncomingSmsMessagesControllerTest < ActionController::TestCase
     expects_invalid_location("FOOBAR")
     SmsMessagingService.any_instance.expects(:send_message).with(user.mobile_phone, OutgoingSmsMessages.bad_location_at_signup)
 
-    assert_no_difference 'User.count' do
-      assert_no_difference 'UserLocation.count' do
-        post :index, :mobile_phone => user.mobile_phone, :message => ' aurora foobar ', :keyword => 'AURORA'
-      end
+    assert_no_new_user do
+      post :index, :mobile_phone => user.mobile_phone, :message => ' aurora foobar ', :keyword => 'AURORA'
     end
-
     assert_response :success
     assert_equal "55419", user.user_location.reload.postal_code
+  end
+
+  private
+
+  def assert_new_user
+    assert_difference 'User.count', 1 do
+      assert_difference 'UserLocation.count', 1 do
+        yield
+      end
+    end
+  end
+
+  def assert_no_new_user
+    assert_no_difference 'User.count' do
+      assert_no_difference 'UserLocation.count' do
+        yield
+      end
+    end
+  end
+
+  def assert_user_deleted
+    assert_difference 'User.count', -1 do
+      assert_difference 'UserLocation.count', -1 do
+        yield
+      end
+    end
   end
 
 end
