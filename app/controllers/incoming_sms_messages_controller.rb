44fc050cdb738f61a72eec97e1e8ae0ee0875c15
John Wood <john@johnpwood.net>
2012-07-28 10:39:06 -0500

    Move incoming SMS processing logic into a series of handler classes

--- app/controllers/incoming_sms_messages_controller.rb
+++ app/controllers/incoming_sms_messages_controller.rb
@@ -2,126 +2,25 @@ class IncomingSmsMessagesController < ApplicationController
   http_basic_authenticate_with :name => ENV['SIGNAL_RECEIVE_SMS_USERNAME'], :password => ENV['SIGNAL_RECEIVE_SMS_PASSWORD']
 
   def index
-    @mobile_phone = params[:mobile_phone]
-    @message = (params[:message] && params[:message].strip)
-    @keyword = (params[:keyword] && params[:keyword].strip)
-    @sms_messaging_service = SmsMessagingService.new
+    mobile_phone = params[:mobile_phone]
+    message = (params[:message] && params[:message].strip)
+    keyword = (params[:keyword] && params[:keyword].strip)
+    user = User.find_by_mobile_phone(mobile_phone)
 
-    @user = User.find_by_mobile_phone(@mobile_phone)
+    handlers = [
+      StopMessageHandler.new(mobile_phone, message, keyword, user),
+      UnknownUserHandler.new(mobile_phone, message, keyword, user),
+      KnownUserHandler.new(mobile_phone, message, keyword, user)
+    ]
 
-    if stop_message?
-      handle_stop_message
-    else
-      if @user.nil?
-        handle_unknown_user
-      else
-        handle_known_user
-      end
+    handled = false
+    handlers.each do |handler|
+      handled = handler.handle
+      break if handled
     end
 
+    Rails.logger.error "Unable to handle message \"#{message}\" from mobile phone #{mobile_phone}" unless handled
     render :nothing => true
-  end
-
-  private
-
-  def handle_unknown_user
-    if opt_in_via_sms_missing_location?
-      handle_opt_in_via_sms_missing_location
-    elsif opt_in_via_sms?
-      handle_opt_in_via_sms
-    else
-      Rails.logger.error "No user could be found with a mobile phone of #{@mobile_phone}"
-    end
-  end
-
-  def handle_known_user
-    if signup_confirmation?
-      handle_signup_confirmation
-    elsif opt_in_via_sms?
-      update_location_for_confirmed_user
-    else
-      handle_already_signed_up
-    end
-  end
-
-  def stop_message?
-    @message.upcase =~ /STOP/
-  end
-
-  def handle_stop_message
-    @user.destroy if @user
-    @sms_messaging_service.send_message(@mobile_phone, OutgoingSmsMessages.stop)
-  end
-
-  def signup_confirmation?
-    positive_response? && !@user.confirmed?
-  end
-
-  def handle_signup_confirmation
-    @user.confirmed_at = Time.now
-    @user.save
-    @sms_messaging_service.send_message(@mobile_phone, OutgoingSmsMessages.signup_confirmation)
-  end
-
-  def positive_response?
-    ["Y", "YES", "SURE", "OK", "YEP", "CONFIRM"].include?(@message.upcase)
-  end
-
-  def opt_in_via_sms_missing_location?
-    @message.upcase == "AURORA"
-  end
-
-  def handle_opt_in_via_sms_missing_location
-    @sms_messaging_service.send_message(@mobile_phone, OutgoingSmsMessages.location_prompt)
-  end
-
-  def opt_in_via_sms?
-    @message.upcase =~ opt_in_message_regexp
-  end
-
-  def handle_opt_in_via_sms
-    factory = UserFactory.new
-    @message.upcase =~ opt_in_message_regexp
-    @user = factory.create_user(@mobile_phone, ($1 || "").strip)
-    @errors = factory.errors
-
-    if @errors.blank?
-      response_message = OutgoingSmsMessages.signup_confirmation
-    elsif factory.location && factory.location.international?
-      response_message = OutgoingSmsMessages.international_location
-    else
-      response_message = OutgoingSmsMessages.bad_location_at_signup
-    end
-    @sms_messaging_service.send_message(@mobile_phone, response_message)
-  end
-
-  def opt_in_message_regexp
-    @opt_in_message_regexp ||= Regexp.new("^AURORA\s+(.*)$", Regexp::IGNORECASE)
-  end
-
-  def handle_already_signed_up
-    @sms_messaging_service.send_message(@mobile_phone, OutgoingSmsMessages.already_signed_up)
-  end
-
-  def update_location_for_confirmed_user
-    @message.upcase =~ opt_in_message_regexp
-    service = GeolocationService.new
-    location = service.geocode($1)
-
-    if location.nil? || location.invalid?
-      @sms_messaging_service.send_message(@mobile_phone, OutgoingSmsMessages.bad_location_at_signup)
-    elsif location.international?
-      @sms_messaging_service.send_message(@mobile_phone, OutgoingSmsMessages.international_location)
-    else
-      @user.user_location.update_attributes(:city => location.city,
-                                            :state => location.state,
-                                            :postal_code => location.zip,
-                                            :country => location.country_code,
-                                            :latitude => location.latitude,
-                                            :longitude => location.longitude,
-                                            :magnetic_latitude => location.magnetic_latitude)
-      @sms_messaging_service.send_message(@mobile_phone, OutgoingSmsMessages.location_update(location.zip))
-    end
   end
 
 end
---
+++ app/models/message_handler.rb
@@ -0,0 +1,25 @@
+class MessageHandler
+
+  def initialize(mobile_phone, message, keyword, user)
+    @mobile_phone = mobile_phone
+    @message = message
+    @keyword = keyword
+    @user = user
+    @sms_messaging_service = SmsMessagingService.new
+  end
+
+  def handle
+    raise "Should be implemented by subclass"
+  end
+
+  protected
+
+  def opt_in_message_regexp
+    @opt_in_message_regexp ||= Regexp.new("^AURORA\s+(.*)$", Regexp::IGNORECASE)
+  end
+
+  def opt_in_via_sms?
+    @message.upcase =~ opt_in_message_regexp
+  end
+
+end
