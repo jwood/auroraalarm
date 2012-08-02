ce6c47d98b1fdcf93fec22752c68b3dbf30159d4
John Wood <john@johnpwood.net>
2012-08-02 07:10:18 -0500

    Add class that will check the local weather for cloud coverage

--- Gemfile
+++ Gemfile
@@ -11,9 +11,9 @@ gem 'dalli', '2.1.0'
 gem 'signal_api', :git => 'git://github.com/signal/signal-ruby.git'
 gem 'proby', '2.2.0'
 gem 'exception_notification', '2.6.1', :require => 'exception_notifier'
-
 gem 'moonphase', :git => 'git://github.com/chaserx/moonphase.git'
 gem 'RubySunrise', '0.3', :require => 'solareventcalculator'
+gem 'nokogiri', '1.5.5'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ app/models/local_weather_service.rb
@@ -0,0 +1,23 @@
+class LocalWeatherService
+  include HttpGetter
+
+  def cloud_cover_percentage(user)
+    data = http_get(request_url(user.user_location))
+    doc = Nokogiri::XML(data)
+    element = doc.xpath('//cloud-amount/value').first
+    element ? element.text.to_i : nil
+  end
+
+  private
+
+  def request_url(user_location)
+    url =  "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php?"
+    url << "lat=#{user_location.latitude}"
+    url << "&lon=#{user_location.longitude}"
+    url << "&product=time-series"
+    url << "&Unit=e"
+    url << "&sky=sky"
+    url
+  end
+
+end
