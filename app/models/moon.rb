93b36cd6ad3170544f16380b5412a4a466716856
John Wood <john@johnpwood.net>
2012-08-06 18:41:32 -0500

    Pull moon phase / symbol mapping into a separate method that can be reused

--- app/models/moon.rb
+++ app/models/moon.rb
@@ -2,16 +2,20 @@ class Moon
 
   def phase(time)
     p = Moonphase::Moon.new.getphase(time)
-    case p
-    when "new (totally dark)"                         then :new
-    when "waxing crescent (increasing to full)"       then :waxing_crescent
-    when "in its first quarter (increasing to full)"  then :first_quarter
-    when "waxing gibbous (increasing to full)"        then :waxing_gibbous
-    when "full (full light)"                          then :full
-    when "waning gibbous (decreasing from full)"      then :waning_gibbous
-    when "in its last quarter (decreasing from full)" then :third_quarter
-    when "waning crescent (decreasing from full)"     then :waning_crescent
-    end
+    Moon.phases[p]
+  end
+
+  def self.phases
+    {
+      "new (totally dark)"                         => :new,
+      "waxing crescent (increasing to full)"       => :waxing_crescent,
+      "in its first quarter (increasing to full)"  => :first_quarter,
+      "waxing gibbous (increasing to full)"        => :waxing_gibbous,
+      "full (full light)"                          => :full,
+      "waning gibbous (decreasing from full)"      => :waning_gibbous,
+      "in its last quarter (decreasing from full)" => :third_quarter,
+      "waning crescent (decreasing from full)"     => :waning_crescent
+    }
   end
 
 end
