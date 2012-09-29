5aa09494ae5e3ee61063b397702d39daf9a6345a
John Wood <john@johnpwood.net>
2012-09-28 21:07:21 -0500

    Use Enumerable#any? instead of essentially re-implementing it.

--- app/models/incoming_sms_handler.rb
+++ app/models/incoming_sms_handler.rb
@@ -23,12 +23,7 @@ class IncomingSmsHandler
       IncomingSmsHandlers::KnownUserHandler.new(@mobile_phone, @message, user, @sms_messaging_service)
     ]
 
-    handled = false
-    handlers.each do |handler|
-      handled = handler.handle
-      break if handled
-    end
-
+    handled = handlers.any? { |handler| handler.handle }
     if !handled
       @sms_messaging_service.send_message(@mobile_phone, OutgoingSmsMessages.unknown_request)
       Rails.logger.error "Unable to handle message \"#{@message}\" from mobile phone #{@mobile_phone}"
