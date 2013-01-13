81ac75856c6c8bdc396128dd05db19cb749981dd
John Wood <john@johnpwood.net>
2013-01-13 15:02:55 -0600

    Move concerns into app/models/concerns

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Aurora
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/finders #{config.root}/app/services)
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/finders #{config.root}/app/services #{config.root}/app/models/concerns)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
