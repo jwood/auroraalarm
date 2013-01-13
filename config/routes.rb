33fe3c88de3d6131af646eebecf28722e8afff32
John Wood <john@johnpwood.net>
2013-01-13 14:57:00 -0600

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Aurora::Application.routes.draw do
-  match '/new_user' => 'site#new_user', :via => :post, :as => :new_user
+  post '/new_user' => 'site#new_user', :as => :new_user
 
-  match '/incoming_sms_messages' => 'incoming_sms_messages#index', :via => :post, :as => :incoming_sms_messages
+  post '/incoming_sms_messages' => 'incoming_sms_messages#index', :as => :incoming_sms_messages
 
-  match '/cron/alert_users_of_solar_event' => 'cron#alert_users_of_solar_event', :via => :post, :as => :cron_alert_users_of_solar_event
-  match '/cron/alert_users_of_aurora' => 'cron#alert_users_of_aurora', :via => :post, :as => :cron_alert_users_of_aurora
-  match '/cron/cleanup' => 'cron#cleanup', :via => :post, :as => :cron_cleanup
+  post '/cron/alert_users_of_solar_event' => 'cron#alert_users_of_solar_event', :as => :cron_alert_users_of_solar_event
+  post '/cron/alert_users_of_aurora' => 'cron#alert_users_of_aurora', :as => :cron_alert_users_of_aurora
+  post '/cron/cleanup' => 'cron#cleanup', :as => :cron_cleanup
 
-  match '/test' => 'test#index', :via => :get
-  match '/test/alert_users_of_solar_event' => 'test#alert_users_of_solar_event', :via => :post
-  match '/test/alert_users_of_aurora' => 'test#alert_users_of_aurora', :via => :post
-  match '/test/send_message' => 'test#send_message', :via => :post
-  match '/test/status' => 'test#status', :via => :get
+  get '/test' => 'test#index'
+  post '/test/alert_users_of_solar_event' => 'test#alert_users_of_solar_event'
+  post '/test/alert_users_of_aurora' => 'test#alert_users_of_aurora'
+  post '/test/send_message' => 'test#send_message'
+  get '/test/status' => 'test#status'
 
   root :to => 'site#index'
 end
