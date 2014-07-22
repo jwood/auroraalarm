47fffb98cf996dbf7da8f89b9eeb481a2fe459ba
John Wood <john@johnpwood.net>
2014-07-22 08:49:32 -0500

    Revert "Have the CronController create background jobs to alert users of solar events and alert users of aurora if conditions optimal"
    This reverts commit 56c372055231d7cb3083433787124ca75d9a45e5.

--- app/controllers/cron_controller.rb
+++ app/controllers/cron_controller.rb
@@ -2,14 +2,14 @@ class CronController < PrivateController
 
   def alert_users_of_solar_event
     Proby.monitor(ENV['PROBY_ALERT_USERS_OF_SOLAR_EVENT']) do
-      SpaceWeatherMonitor.delay.alert_users_of_solar_event
+      SpaceWeatherMonitor.alert_users_of_solar_event
       render nothing: true
     end
   end
 
   def alert_users_of_aurora
     Proby.monitor(ENV['PROBY_ALERT_USERS_OF_AURORA']) do
-      AuroraConditionsMonitor.delay.alert_users_of_aurora_if_conditions_optimal
+      AuroraConditionsMonitor.alert_users_of_aurora_if_conditions_optimal
       render nothing: true
     end
   end
