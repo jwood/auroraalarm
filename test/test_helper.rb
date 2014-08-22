d9f2273b8a2012c9503d4bf1ea684789b66ba615
John Wood <john@johnpwood.net>
2014-08-21 21:23:06 -0500

    Upgrade to Rails 4.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,6 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-require 'test/unit'
 require 'mocha/setup'
 
 class ActiveSupport::TestCase
